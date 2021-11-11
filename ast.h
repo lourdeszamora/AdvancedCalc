02adf891a44d5dbcda1ce935b02b826e6a0b85e1
lourdeszamora <lourdes.zamora@unitec.edu>
2021-11-10 20:38:18 -0600

    progress

--- ast.h
+++ ast.h
@@ -6,4 +6,173 @@
 
 using namespace std;
 
+class Expr;
+class InitDeclarator;
+class Statement;
+class Declaration;
+class Parameter;
+typedef list<Expr *> InitializerElementList;
+typedef list<InitDeclarator *> InitDeclaratorList;
+typedef list<Expr *> ParameterList;
+typedef list<Statement *> StatementList;
+typedef list<Expr *> ArgumentList;
+typedef list<Declaration *> DeclarationList;
+
+class Statement{
+    public:
+        int line;
+        virtual void printResult() = 0;
+};
+
+class Expr{
+    public:
+        int line;
+        virtual float getResult() = 0;
+};
+
+class Initializer{
+    public:
+        Initializer(InitializerElementList expressions, int line){
+            this->expressions = expressions;
+            this->line = line;
+        }
+        InitializerElementList expressions;
+        int line;
+};
+
+class Declarator{
+    public:
+        Declarator(string id, Expr* arrayDeclaration,  int line){
+            this->id = id;
+            this->line = line;
+            this->arrayDeclaration = arrayDeclaration;
+        }
+        string id;
+        int line;
+        Expr * arrayDeclaration;
+};
+
+class InitDeclarator{
+    public:
+        InitDeclarator(Declarator * declarator, Initializer * initializer, int line){
+            this->declarator = declarator;
+            this->initializer = initializer;
+            this->line = line;
+        }
+        Declarator * declarator;
+        Initializer * initializer;
+        int line;
+};
+
+class Declaration{
+    public:
+        Declaration( InitDeclaratorList declarations, int line){
+            this->declarations = declarations;
+            this->line = line;
+        }
+        InitDeclaratorList declarations;
+        int line;
+        int evaluateSemantic();
+};
+
+class BlockStatement : public Statement{
+    public:
+        BlockStatement(StatementList statements, DeclarationList declarations, int line){
+            this->statements = statements;
+            this->declarations = declarations;
+            this->line = line;
+        }
+        StatementList statements;
+        DeclarationList declarations;
+        int line;
+        void printResult();
+};
+
+class MethodDefinition : public Statement{
+    public:
+        MethodDefinition(string id, ParameterList params, Statement * statement, int line){
+            this->id = id;
+            this->params = params;
+            this->statement = statement;
+            this->line = line;
+        }
+
+        string id;
+        ParameterList params;
+        Statement * statement;
+        int line;
+        void printResult();
+};
+
+class FloatExpr : public Expr{
+    public:
+        FloatExpr(float value, int line){
+            this->value = value;
+            this->line = line;
+        }
+        float value;
+        float getResult();
+};
+
+class BinaryExpr : public Expr{
+    public:
+        BinaryExpr(char symbol ,Expr * expr1, Expr *expr2, int line){
+            this->expr1 = expr1;
+            this->expr2 = expr2;
+            this->line = line;
+            this->symbol = symbol;
+        }
+        Expr * expr1;
+        Expr *expr2;
+        char symbol;
+        int line;
+        float getResult();
+};
+
+class IdExpr : public Expr{
+    public:
+        IdExpr(string id, int line){
+            this->id = id;
+            this->line = line;
+        }
+        string id;
+        int line;
+        float getResult();
+};
+
+class MethodInvocationStmt : public Statement{
+    public:
+        MethodInvocationStmt(string id, ParameterList args, int line){
+            this->id = id;
+            this->args = args;
+            this->line = line;
+        }
+        string id;
+        ArgumentList args;
+        int line;
+        void printResult();
+};
+
+class WhileStatement: public Statement{
+    public:
+        WhileStatement(Expr * expr, Statement * stmt, int line){
+            this->expr = expr;
+            this->stmt = stmt;
+            this->line = line;
+        }
+        Expr* expr;
+        Statement * stmt;
+        int line;
+        void printResult();
+};
+
+class ExprStatement : public Statement{
+    public:
+        ExprStatement(Expr * expr, int line){
+            this->expr = expr;
+            this->line = line;
+        }
+        Expr * expr;
+        void printResult();
+};
 #endif
--- calc.y
+++ calc.y
@@ -16,13 +16,78 @@
 %}
 
 %union{
-  
+    const char * string_t;
+    float float_t;
+    Expr * expr_t;
+    Statement * statement_t;
 }
 
 %token EOL
 %token ADD SUB MUL DIV IF THEN ENDIF WHILE DO DONE ELSE LET
+%token<string_t> TK_ID
+%token<float_t>  TK_LIT_FLOAT
+
+%type <statement_t> while_stmt
+%type<expr_t> term
 
 %%
-start:
-     ;
+start: input{
+    list<Statement *>::iterator it = $1->begin();
+    while(it != $1->end()){
+        printf("semantic result: %d \n",(*it)->printResult());
+        it++;
+    }
+}
+
+input: /* empty */ 
+    | input method_decl
+    | input variable_decl
+    | input expr_stmt
+    | input while_stmt
+    | input method_invoc;
+    ;
+
+method_decl: LET TK_ID '(' param_list ')' '=' statements ';' { 
+                    $$ = new MethodDefinition( $2, *$4, $6, yylineno );
+                    delete $4;
+                    };
+
+param_list: param_list ',' TK_ID {$$ = $1; $$->push_back(new IdExpr($3, yylineo));}
+            | TK_ID  { $$ = new ParameterList; $$->push_back(new IdExpr($1, yylineno)); }
+            ;
+
+statements:  statement { $$ = new StatementList; $$->push_back($1); }
+    | statements statement ';'  { $$ = $1; $$->push_back($2); }
+    ;
+
+statement: variable_decl {$$ = $1;}
+    |expr_stmt {$$ = $1;}
+    | while_stmt {$$ = $1;}
+    | method_invoc {$$ = $1;}
+    ;
+
+method_invoc: TK_ID '(' param_list ')' { $$ = new MethodInvocationStmt($1, *$3, yylineo); }
+
+variable_decl: LET TK_ID '=' expr_stmt { $$ = new Declarator($1,$3,yylineno); }
+
+expr_stmt:  expr_stmt ADD factor { $$ = new BinaryExpr( $2, $1, $3, yylineno); }
+    | expr_stmt SUB factor { $$ = new BinaryExpr( $2, $1, $3, yylineno); }
+    | factor { $$ = $1 }
+    ;
+
+factor: factor MUL term { $$ = new BinaryExpr( $2, $1, $3, yylineno); }
+    | factor DIV term { $$ = new BinaryExpr( $2, $1, $3, yylineno); }
+    | term { $$ = $1 }
+    ;
+
+term: TK_LIT_FLOAT {$$ = new FloatExpr($1, yylineo); }
+    | TK_ID { $$ = new IdExpr($1, yylineno); }
+    ;
+
+while_stmt: WHILE '(' rel_expr ')' DO statements DONE { $$ = new WhileStatement( $3, $6, yylineno); }
+
+rel_expr: term '>' term { $$ = new BinaryExpr( $2, $1, $3, yylineno); }
+        | term '<' term { $$ = new BinaryExpr( $2, $1, $3, yylineno); }
+        ;
+
 %%
